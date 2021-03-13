125484c017dc29586a5381ebb3d3cfe6e16ea94e
Kadrangelion <j.kasmi@proxiad.com>
2021-03-13 10:31:00 +0100

    feat: adding a loader component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Loader from './Loader';
 import Searchbar from './Searchbar';
 import Switch from './Switch';
 
 export {
   Header,
+  Loader,
   Searchbar,
   Switch,
 }
